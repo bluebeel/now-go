141d8dceaf8c057fe6f84be0ad5a99017a18a4d7
bluebeel <saikouah@gmail.com>
2018-11-28 22:56:01 +0100

    update build script

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 mkdir -p bin
 cd util
-GOOS=linux GOARCH=amd64 go build get-exported-function-name.go
+if [ "{$OS}" == "windowsnt" ]; then
+    GOOS=windows GOARCH=amd64  go build get-exported-function-name.go
+elif [ "{$OS}" == "darwin" ]; then
+    GOOS=darwin GOARCH=amd64 go build get-exported-function-name.go
+else
+    GOOS=linux GOARCH=amd64 go build get-exported-function-name.go
+if
 mv get-exported-function-name ../bin/
